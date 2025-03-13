e5e9dcf0675eb7631d768e3066f5decf060d689c
nikammayu2800 <mayurinikam2800@gmail.com>
2025-03-13 10:54:07 -0400

    Problem 4 FizzBuzz Solution.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,14 @@
 import { add } from '../index';
+import { fizzBuzz } from '../index';
 
 describe('add function', () => {
   test('should add two numbers correctly', () => {
     expect(add(1, 1)).toBe(2);
+  });
+});
+
+describe('check FizzBuzz', () => {
+  test('Should print Fizz for 12', () => {
+    expect(fizzBuzz(12)).toBe("Fizz");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,24 @@ export function add(a: number, b: number): number {
 
 console.log("job done.");
 
+// Problem 4:
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true
+
+export function fizzBuzz(num: number): string {
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "FizzBuzz";
+    } else if (num % 3 === 0){
+      return "Fizz";
+    } else if (num % 5 === 0) {
+      return "Buzz";
+    } else {
+      return num.toString();
+    }
+}
+
+fizzBuzz(12);
+
